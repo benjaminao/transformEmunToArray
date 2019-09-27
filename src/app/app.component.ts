876055eb2ad34efa67a889f66f412a3a44f06872
Benjamin Alvarez <benjaminalvarez@Benjamins-MBP.duke-energy.com>
2019-09-27 08:22:31 -0400

    Added method to transform enum into array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,22 @@
 import { Component } from '@angular/core';
-
+export enum DesignVersionStatusCodes {
+  ACCEPTED= 'Accepted',
+  ESTIMATED= 'Estimated',
+  RDYFOREST= 'Ready For Estimation',
+  NEW= 'New - Not Estimated',
+  ASYNC= 'Running in Asynchronous Task',
+  QUOTED= 'Quoted to external entity',
+  RDYFORACC= 'Ready for Acceptance',
+  ERROR='Estimated with Errors',
+  SENDQUEUE= 'Sent to Queue',
+  SENDMAX= 'Sent to Maximo',
+  CLMCONTAPPR= 'CLM Contract Approved',
+  MISMATCH= 'Mismatch',
+  RDYPPLNEST= 'Ready to Estimate Power Plan',
+  TOBEACCEPTED= 'CUE Version to be Accepted by CLM',
+  WAITFORACC= 'Waiting for Nightly Acceptance Process',
+  WAITFOREST= 'Waiting for Nightly Estimation Process'
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +24,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'listApp';
+  list = DesignVersionStatusCodes;
+  mylist = [];
+  selectedValue: any;
+  constructor() {
+    console.log('list ', this.list);
+    this.transformEmunToArray(this.list);
+    console.log('mylist ', this.mylist);
+  }
+
+  transformEmunToArray(enumObject) {
+
+    console.log('enumObject ', enumObject);
+
+    for (const key in enumObject) {
+      if (key) {
+        this.mylist.push({key: key, value: enumObject[key]});
+      }
+    }
+    enumObject.all = this.mylist;
+ }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatSelectModule, MatChipsModule, MatCheckboxModule, MatCardModule } from '@angular/material';
+
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserModule,
+    MatSelectModule,
+    MatChipsModule,
+    MatCheckboxModule,
+    MatCardModule,
+    BrowserAnimationsModule
+  ],
+  exports: [
+    MatSelectModule,
+    MatChipsModule,
+    MatCardModule,
+    MatCheckboxModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
